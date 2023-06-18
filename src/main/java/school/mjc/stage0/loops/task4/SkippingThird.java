1933adb76619c0efde988320b82c3000c795ddda
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-18 17:29:14 -0400

    Update SkippingThird.java

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task4;
 
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
+ int n = 0;
+        for (int t = 0; t <= lastPrinted; t++) {
+            n++;
+            if (n % 3 == 0) {
+                continue;
+            } else {
+                System.out.println(t);
+            }
+        }
 
     }
 }
